f4e70b623dbe1735a6b899ffe8c698a5717306ff
Denis Khashchuk <khashchuk.denis@gmail.com>
2022-01-09 20:15:00 +0300

    Add files via upload

--- uint1024_t/main.c
+++ uint1024_t/main.c
@@ -1,169 +1,154 @@
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
+#include <stdlib.h>
 
-#define  MAX_LENGTH 309
-#define DIV 1000000000
+const int DIV = 1000000000;
 
 struct uint1024_t {
-    int array[35];
+    unsigned int array[35];
+    int blocks;
 };
 
 void make_inited(struct uint1024_t *x) {
     for (int i = 0; i < 35; i++)
         x->array[i] = 0;
-}
-
-struct uint1024_t add_op(struct uint1024_t x, struct uint1024_t y) {
-    struct uint1024_t sum;
-    make_inited(&sum);
-    int add = 0;
-    for (int i = 34; i > -1; --i) {
-        unsigned long long int sm = x.array[i] + y.array[i] + add;
-        sum.array[i] = sm % DIV;
-        add = sm / DIV;
-    }
-    return sum;
-}
-
-struct uint1024_t subtr_op(struct uint1024_t x, struct uint1024_t y) {
-    struct uint1024_t subtr;
-    int debt = 0;
-    for (int i = 34; i > -1; --i) {
-        if (x.array[i] >= y.array[i] + debt) {
-            subtr.array[i] = x.array[i] - y.array[i] - debt;
-            debt = 0;
-        } else {
-            subtr.array[i] = x.array[i] + DIV - y.array[i] - debt;
-            debt = 1;
-        }
-    }
-    if (debt) {
-        make_inited(&subtr);
-        printf("WRONG ANSWER!!! THE RESULT OF SUBSTRACTION IS BELOW ZERO\n");
-        return subtr;
-    } else {
-        return subtr;
-    }
-
+    x->blocks = 0;
 }
 
 struct uint1024_t from_uint(unsigned int x) {
-    struct uint1024_t uintStructure;
-    make_inited(&uintStructure);
-    if (x / DIV != 0) {
-        uintStructure.array[33] = x / DIV;
-        uintStructure.array[34] = x % DIV;
-    } else {
-        uintStructure.array[34] = x;
-    }
-    return uintStructure;
+    struct uint1024_t number;
+    for (int i = 0; i < 35; ++i)
+        number.array[i] = 0;
+    number.array[0] = x % DIV;
+    if (x >= DIV)
+        number.blocks = 2;
+    else
+        number.blocks = 1;
+    if (number.blocks > 1)
+        number.array[1] = (unsigned int) (x / DIV);
+    return number;
 }
 
-int was_multiplicified(struct uint1024_t x) {
-    for (int i = 0; i < 35; ++i) {
-        if (x.array[i] != 0)
-            return 0;
-    }
-    return 1;
-}
+struct uint1024_t add_op(struct uint1024_t x, struct uint1024_t y) {
+    struct uint1024_t result;
+    int additive = 0;
+    for (int i = 0; i < 35; ++i)
+        result.array[i] = 0;
 
-struct uint1024_t mult_op(struct uint1024_t x, struct uint1024_t y) {
-//    struct uint1024_t multiplification = from_uint(0);
-//    make_inited(&multiplification);
-//    struct uint1024_t decrement = from_uint(1);
-//    while (!was_multiplicified(y)) {
-//        y = subtr_op(y, decrement);
-//        multiplification = add_op(multiplification, x);
-//    }
-  //  return multiplification;
-    struct uint1024_t mult = from_uint(0);
-    for(int i = 34; i > -1; --i){
-         int additive = 0;
-        for ( int j = 34; i != -1; i--){
-            if (i + j > 35) break;
-            unsigned long t =mult.array[i + j] + x.array[i] * y.array[i] + additive;
-            mult.array[i + j]= t % DIV;
-            additive = t / DIV;
+    for (int i = 0; i < 35; i++) {
+        result.array[i] = x.array[i] + additive + y.array[i];
+        if (result.array[i] >= DIV) {
+            additive = result.array[i];
+            result.array[i] -= DIV;
         }
     }
-    return mult;
-}
 
-void printf_value(struct uint1024_t x) {
-    int flag = -1;
-    for (int i = 0; i < 35; ++i) {
-        if (x.array[i] != 0) {
-            flag = i;
+    for (int j = 34; j >= 0; --j) {
+        if (result.array[j] != 0) {
+            result.blocks = j + 1;
             break;
         }
     }
-    if (flag == -1) {
-        printf("0\n");
-    } else {
-        printf("%d", x.array[flag]);
-        if (flag + 1 < 35)
-            for (int i = flag + 1; i < 35; ++i)
-                printf("%09d", x.array[i]);
-        printf("\n");
+    return result;
+}
+
+struct uint1024_t subtr_op(struct uint1024_t x, struct uint1024_t y) {
+    struct uint1024_t result;
+    int temp = 0;
+    make_inited(&result);
+    for (int i = 0; i < x.blocks; ++i) {
+        result.array[i] = x.array[i] - temp;
+        if (i < y.blocks)
+            result.array[i] -= y.array[i];
+        if (result.array[i] < 0) {
+            temp = x.array[i];
+            result.array[i] += DIV;
+        }
+    }
+    for (int j = 34; j >= 0; --j) {
+        if (result.array[j] != 0) {
+            result.blocks = j + 1;
+            break;
+        }
+    }
+    return result;
+}
+
+void printf_value(struct uint1024_t x) {
+    if (x.blocks) printf("%u", x.array[x.blocks - 1]); else printf("0\n");
+    for (int i = x.blocks - 2; i > -1; --i)
+        printf("%09u", x.array[i]);
+    printf("\n");
+}
+
+struct uint1024_t mult_op(struct uint1024_t x, struct uint1024_t y) {
+    struct uint1024_t result;
+    for (int i = 0; i < 35; ++i)
+        result.array[i] = 0;
+    for (int i = 0; i < 35; ++i)
+        for (int j = 0, flag = 0; 35 > i + j; ++j) {
+            long long current = result.array[i + j] + x.array[i] * 1ll * y.array[j] + flag;
+            result.array[i + j] = (int) (current % DIV);
+            flag = (int) (current / DIV);
+            if (flag && i == 34)
+                break;
+        }
+    for (int k = 34; k >-1; --k) {
+        if (result.array[k] != 0) {
+            result.blocks = k + 1;
+            break;
+        }
+    }
+    return result;
+}
+
+void init_number_of_blocks(struct uint1024_t *x, char *string) {
+    int numberOfBlocks = strlen(string);
+    for (int k = numberOfBlocks; k > 0; k -= 9) {
+        string[k] = '\0';
+        if (k >= 9)
+            x->array[x->blocks] = (unsigned int) (atoi(string + k - 9));
+        else
+            x->array[x->blocks] = (unsigned int) (atoi(string));
+        x->blocks += 1;
     }
 }
 
-int convert_block_to_int(char array[], int i1) {
-    int number = 0;
-    int pw = 0;
-    if (i1 != 9) {
-        pw = 0;
-        for (int i = 0; pw != i1; ++i) {
-            number += (array[i] - '0') * pow(10, pw++);
+struct uint1024_t pow_op(struct uint1024_t x, int power) {
+    struct uint1024_t result;
+    make_inited(&result);
+    result.array[0] = 1;
+    result.blocks = 1;
+    struct uint1024_t temp = x;
+    power++;
+    while (power) {
+        if (power % 2 == 0) {
+            power /= 2;
+            temp = mult_op(temp, x);
+        } else {
+            power--;
+            result = mult_op(result, temp);
         }
-        return number;
     }
-    for (int i = 0; pw != strlen(array); ++i) {
-        number += (array[i] - '0') * pow(10, pw++);
-    }
-    return number;
+    return result;
 }
 
 void scanf_value(struct uint1024_t *x) {
     make_inited(x);
-    char tempArray[MAX_LENGTH];
-    //while (1) {
-    scanf("%s", &tempArray);
-    //  if (tempArray[0] == '0' || tempArray[0] == '-') printf("THE ENTERED VALUE IS INVALID\n"); else break;
-    //}
-    int length = strlen(tempArray);
-    int blocksToFill;
-    if (length % 9 == 0) blocksToFill = length / 9; else blocksToFill = length / 9 + 1;
-    char blockToconvert[10];
-    int filler = 34;
-    int digits = 0;
-    for (int i = length - 1; i > -1; --i) {
-        if (digits == 9) {
-            blockToconvert[9] = '\0';
-            x->array[filler--] = convert_block_to_int(blockToconvert, digits);
-            blocksToFill--;
-            digits = 0;
-        }
-        blockToconvert[digits++] = tempArray[i];
-    }
-    if (blocksToFill != 0) {
-        x->array[filler] = convert_block_to_int(blockToconvert, digits);
-    }
-//    for(int i = 0; i <35; ++i){
-//        printf("%d\n",x->array[i]);
-//    }
+    char *input = (char *) (calloc(310, sizeof(char)));
+    scanf("%s", input);
+    init_number_of_blocks(x, input);
 }
 
 int main() {
     struct uint1024_t x, y;
     scanf_value(&x);
     scanf_value(&y);
+    printf("mult_op:\n");
     printf_value(mult_op(x, y));
-    printf_value(add_op(x, y));
-    printf_value(subtr_op(x, y));
-    unsigned int number;
-    scanf("%u", &number);
-    printf_value(from_uint(number));
+    printf("\npow_op:\n");
+    printf_value(pow_op(x, 5));
     return 0;
 }
+
+
